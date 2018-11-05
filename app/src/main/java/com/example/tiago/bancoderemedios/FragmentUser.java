a52089c15017ab3f68253c992dac1394d849584c
tofarias <tiago.farias.poa@gmail.com>
2018-11-05 12:13:03 -0200

    Add menu lateral

---
+++ app/src/main/java/com/example/tiago/bancoderemedios/FragmentUser.java
@@ -0,0 +1,24 @@
+package com.example.tiago.bancoderemedios;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentUser extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle(R.string.nav_header_user);
+    }
+}
