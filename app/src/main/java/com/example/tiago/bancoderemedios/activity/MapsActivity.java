233bd55f62437e3bc3457f8b0c0843bf89ef461d
tofarias <tiago.farias.poa@gmail.com>
2018-11-06 16:47:16 -0200

    Fix zoom

--- app/src/main/java/com/example/tiago/bancoderemedios/activity/MapsActivity.java
+++ app/src/main/java/com/example/tiago/bancoderemedios/activity/MapsActivity.java
@@ -38,10 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        
+        LatLng sydney = new LatLng(-30.028201, -51.227365);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-30.028201, -51.227365), 17f));
     }
 }
