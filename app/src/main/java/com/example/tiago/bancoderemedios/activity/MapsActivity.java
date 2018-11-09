48fc85cfdbd52725a284f35fb7bf273f73d50e1f
tofarias <tiago.farias.poa@gmail.com>
2018-11-09 11:07:40 -0200

    Add exibindo local atual

--- app/src/main/java/com/example/tiago/bancoderemedios/activity/MapsActivity.java
+++ app/src/main/java/com/example/tiago/bancoderemedios/activity/MapsActivity.java
@@ -1,9 +1,17 @@
 package com.example.tiago.bancoderemedios.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.example.tiago.bancoderemedios.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +19,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +32,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        this.mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        
+
         LatLng sydney = new LatLng(-30.028201, -51.227365);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-30.028201, -51.227365), 17f));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        this.mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (this.mGoogleApiClient != null && this.mGoogleApiClient.isConnected()) {
+            this.mGoogleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        this.obterLocalizacao();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        this.mGoogleApiClient.connect();
+    }
+
+    private void obterLocalizacao() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPe rmissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },1);
+
+            return;
+        }else{
+            //FusedLocationProviderClient location = LocationServices.getFusedLocationProviderClient(this);
+            Location location = LocationServices.FusedLocationApi.getLastLocation(this.mGoogleApiClient);
+
+            if( location != null ){
+                this.atualizarMapa(new LatLng(location.getLatitude(), location.getLongitude()));
+            }
+        }
+    }
+
+    private void atualizarMapa(LatLng local){
+        this.mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(local, 17.0f));
+        this.mMap.addMarker(new MarkerOptions().position(local).title("Local Atual!"));
     }
 }
