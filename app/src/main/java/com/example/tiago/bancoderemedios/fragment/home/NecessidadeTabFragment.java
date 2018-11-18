ebcc03494b692ed87ae0a1b8f4bdcaff8883d750
Tiago Farias <tiago.farias.poa@gmail.com>
2018-11-18 18:57:09 -0200

    Add formulário de cadastro de necessidade

--- app/src/main/java/com/example/tiago/bancoderemedios/fragment/home/NecessidadeTabFragment.java
+++ app/src/main/java/com/example/tiago/bancoderemedios/fragment/home/NecessidadeTabFragment.java
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tiago.bancoderemedios.R;
 
 public class NecessidadeTabFragment extends Fragment {
+
+    EditText editTextTitulo;
+    Button btnSalvar;
 
     @Nullable
     @Override
@@ -17,10 +23,23 @@ public class NecessidadeTabFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_necessidade_tab, container, false);
     }
 
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //getActivity().setTitle(R.string.nav_header_user);
+
+        this.editTextTitulo = (EditText) getActivity().findViewById(R.id.editTextTitulo);
+        this.editTextTitulo.requestFocus();
+
+        this.btnSalvar = (Button) getActivity().findViewById(R.id.buttonSalvar);
+        this.btnSalvar.setOnClickListener( this.btnSalvarOnClickListener );
     }
+
+    private View.OnClickListener btnSalvarOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            Toast.makeText(getContext(), "Não Implementado", Toast.LENGTH_LONG).show();
+        }
+    };
 }
